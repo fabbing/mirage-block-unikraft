cf880f10843255424b7a6b234cb4d244adc815a6
fabbing <fabrice@tarides.com>
2025-04-22 14:10:17 +0200

    linting sempahore.ml*

--- src/semaphore.ml
+++ src/semaphore.ml
@@ -24,19 +24,24 @@ type t = {
 let make count =
   if count < 0 then raise (Invalid_argument "Invalid initial count");
   { mutex = Lwt_mutex.create (); count; nonzero = Lwt_condition.create () }
-  
+
 let acquire t =
-  Lwt_mutex.with_lock t.mutex Lwt.(fun () ->
-      let rec aux () =
-        if t.count = 0 then Lwt_condition.wait ~mutex:t.mutex t.nonzero >>= aux
-        else Lwt.return_unit
-      in
-      aux () >>= fun () ->
-      t.count <- t.count - 1;
-      Lwt.return_unit)
+  Lwt_mutex.with_lock t.mutex
+    Lwt.(
+      fun () ->
+        let rec aux () =
+          if t.count = 0 then
+            Lwt_condition.wait ~mutex:t.mutex t.nonzero >>= aux
+          else return_unit
+        in
+        aux () >>= fun () ->
+        t.count <- t.count - 1;
+        return_unit)
 
 let release t =
-  Lwt_mutex.with_lock t.mutex (fun () ->
-      t.count <- t.count + 1;
-      Lwt_condition.signal t.nonzero ();
-      Lwt.return_unit)
+  Lwt_mutex.with_lock t.mutex
+    Lwt.(
+      fun () ->
+        t.count <- t.count + 1;
+        Lwt_condition.signal t.nonzero ();
+        return_unit)
--- src/semaphore.mli
+++ src/semaphore.mli
@@ -18,6 +18,5 @@
 type t
 
 val make : int -> t
-
 val acquire : t -> unit Lwt.t
 val release : t -> unit Lwt.t
